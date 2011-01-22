7b2b34b123e3918a6d9379620546a798d97fae2b
unknown <Carlo@.(none)>
2011-01-22 02:49:57 -0500

    Readded Stuff I deletd

---
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Facebook_demonstration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.screenShotButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // screenShotButton
+            // 
+            this.screenShotButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.screenShotButton.Location = new System.Drawing.Point(0, 0);
+            this.screenShotButton.Name = "screenShotButton";
+            this.screenShotButton.Size = new System.Drawing.Size(162, 62);
+            this.screenShotButton.TabIndex = 5;
+            this.screenShotButton.Text = "Upload Photo";
+            this.screenShotButton.UseVisualStyleBackColor = true;
+            this.screenShotButton.Click += new System.EventHandler(this.screenShotButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(162, 62);
+            this.Controls.Add(this.screenShotButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button screenShotButton;
+    }
+}
+
---
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using Facebook.Rest;
+using Facebook.Schema;
+using Facebook.Utility;
+using Facebook.Winforms.Components;
+using FacebookController;
+using SHDocVw;
+
+namespace Facebook_demonstration
+{
+    public partial class Form1 : Form
+    {
+        #region Class members
+        readonly FacebookService fbService = new FacebookService();
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            fbService.ApplicationKey = "120183071389019";
+            List<Enums.ExtendedPermissions> perms = new List<Enums.ExtendedPermissions>
+            {
+                Enums.ExtendedPermissions.photo_upload,
+                Enums.ExtendedPermissions.offline_access
+            };
+            fbService.ConnectToFacebook(perms);
+        }
+
+        #region Button Handlers
+        private void screenShotButton_Click(object sender, EventArgs e)
+        {
+            PublishPhotos("Test Album 1", "C:/uploadthisfile.png");
+        }
+        #endregion
+
+        #region Facebook Interfaces
+        private static void PublishAsyncCompleted(string result, Object state, FacebookException e)
+        {
+
+        }
+
+        public bool PublishPhotos(String AlbumName, String FileName)
+        {            
+            try
+            {
+                IList<album> albums = fbService.Photos.GetAlbums();
+
+                string albumAid = "";
+                foreach (album album in albums)
+                {
+                    if (album.name == AlbumName)
+                    {
+                        albumAid = album.aid;
+                        break;
+                    }
+                }
+
+                if (albumAid == "")
+                {
+                    albumAid = fbService.Photos.CreateAlbum(AlbumName, null, "Album description").aid;
+                }
+
+                //fbService.Photos.UploadAsync(albumAid, "Uploaded from Facebox", Photo, Enums.FileType.png, UploadCallback, null);
+                fbService.Photos.Upload(albumAid, "Uploaded from Facebox", new FileInfo(FileName));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private void CreateAlbumCallback(album album, object state, FacebookException e)
+        {
+            
+        }
+
+        private void UploadCallback(photo p, object state, FacebookException e)
+        {
+            
+        }
+
+        private void PhotoTagger(string photoPid)
+        {
+            
+        }
+        #endregion
+    }
+}
