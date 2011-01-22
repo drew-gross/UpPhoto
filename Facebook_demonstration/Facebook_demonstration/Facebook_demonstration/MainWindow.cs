382b4cfeaee6f1073b6fca1cdeb2268cdddd2bb7
unknown <Christophe@.(none)>
2011-01-22 12:58:50 -0500

    Updated with ui.

---
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/MainWindow.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FacebookApplication
+{
+    public partial class MainWindow : Form
+    {
+        private List<FileSystemWatcher> watchList = new List<FileSystemWatcher>();
+        private UpdateHandler handler = new UpdateHandler();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddFolderToTrack(string path)
+        {
+            FileSystemWatcher watcher = new FileSystemWatcher
+            {
+                EnableRaisingEvents = true,
+                Filter = "*.*",
+                IncludeSubdirectories = true,
+                NotifyFilter = NotifyFilters.FileName |
+                NotifyFilters.DirectoryName |
+                NotifyFilters.LastAccess,
+                Path = path,
+            };
+
+            watcher.Changed += new FileSystemEventHandler(handler.FaceboxWatcher_Changed);
+            //watcher.Renamed += new FileSystemEventHandler(handler.FaceboxWatcher_Renamed);
+            watcher.Created += new FileSystemEventHandler(handler.FaceboxWatcher_Created);
+            watcher.Deleted += new FileSystemEventHandler(handler.FaceboxWatcher_Deleted);
+            
+            watchList.Add(watcher);
+        }
+
+        public List<string> trackedFolders()
+        {
+            List<string> ret = new List<string>();
+            foreach(FileSystemWatcher watcher in watchList){
+                ret.Add(watcher.Path);
+            }
+            return ret;
+        }
+
+        private void ChangeAccountItem_Click(object sender, EventArgs e)
+        {
+            // Logout, then show prompt again.
+        }
+
+        private void LogoutItem_Click(object sender, EventArgs e)
+        {
+            // TODO: Implement logging out.
+        }
+
+        private void WatchFolderItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (string path in Directory.GetFiles(dialog.SelectedPath))
+                {
+                    AddFolderToTrack(path);
+                }
+            }
+
+        }
+    }
+}
--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Program.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using System.IO;
+using FacebookApplication;
 
 namespace Facebook_demonstration
 {
@@ -16,7 +17,7 @@ namespace Facebook_demonstration
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
