011f6e38776a9274a0e7f5a81be1b6a37b803406
akash <avaswani@uwaterloo.ca>
2011-01-22 03:24:55 -0500

    refactoring form1

---
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/FacebookInterfaces.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Facebook.Rest;
+using Facebook.Schema;
+using Facebook.Utility;
+using Facebook.Winforms.Components;
+using FacebookController;
+using System.IO;
+
+namespace Facebook_demonstration
+{
+    static class FacebookInterfaces
+    {
+        static readonly FacebookService fbService = new FacebookService();
+
+        static void PublishAsyncCompleted(string result, Object state, FacebookException e)
+        {
+
+        }
+
+        public static bool PublishPhotos(String AlbumName, String FileName)
+        {
+            try
+            {
+                IList<album> albums = fbService.Photos.GetAlbums();
+
+                string albumAid = "";
+                foreach (album album in albums)
+                {
+                    if (album.name == AlbumName)
+                    {
+                        albumAid = album.aid;
+                        break;
+                    }
+                }
+
+                if (albumAid == "")
+                {
+                    albumAid = fbService.Photos.CreateAlbum(AlbumName, null, "Album description").aid;
+                }
+
+                //fbService.Photos.UploadAsync(albumAid, "Uploaded from Facebox", Photo, Enums.FileType.png, UploadCallback, null);
+                fbService.Photos.Upload(albumAid, "Uploaded from Facebox", new FileInfo(FileName));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static void CreateAlbumCallback(album album, object state, FacebookException e)
+        {
+
+        }
+
+        private static void UploadCallback(photo p, object state, FacebookException e)
+        {
+
+        }
+
+        private static  void PhotoTagger(string photoPid)
+        {
+
+        }
+    }
+}
--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Form1.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/Form1.cs
@@ -18,6 +18,7 @@ namespace Facebook_demonstration
         readonly FacebookService fbService = new FacebookService();
         #endregion
 
+
         public Form1()
         {
             InitializeComponent();
@@ -33,61 +34,61 @@ namespace Facebook_demonstration
         #region Button Handlers
         private void screenShotButton_Click(object sender, EventArgs e)
         {
-            PublishPhotos("Test Album 1", "C:/uploadthisfile.png");
+            FacebookInterfaces.PublishPhotos("Test Album 1", "C:/uploadthisfile.png");
         }
         #endregion
 
         #region Facebook Interfaces
-        private static void PublishAsyncCompleted(string result, Object state, FacebookException e)
-        {
+        //private static void PublishAsyncCompleted(string result, Object state, FacebookException e)
+        //{
 
-        }
+        //}
 
-        public bool PublishPhotos(String AlbumName, String FileName)
-        {            
-            try
-            {
-                IList<album> albums = fbService.Photos.GetAlbums();
+        //public bool PublishPhotos(String AlbumName, String FileName)
+        //{            
+        //    try
+        //    {
+        //        IList<album> albums = fbService.Photos.GetAlbums();
 
-                string albumAid = "";
-                foreach (album album in albums)
-                {
-                    if (album.name == AlbumName)
-                    {
-                        albumAid = album.aid;
-                        break;
-                    }
-                }
+        //        string albumAid = "";
+        //        foreach (album album in albums)
+        //        {
+        //            if (album.name == AlbumName)
+        //            {
+        //                albumAid = album.aid;
+        //                break;
+        //            }
+        //        }
 
-                if (albumAid == "")
-                {
-                    albumAid = fbService.Photos.CreateAlbum(AlbumName, null, "Album description").aid;
-                }
+        //        if (albumAid == "")
+        //        {
+        //            albumAid = fbService.Photos.CreateAlbum(AlbumName, null, "Album description").aid;
+        //        }
 
-                //fbService.Photos.UploadAsync(albumAid, "Uploaded from Facebox", Photo, Enums.FileType.png, UploadCallback, null);
-                fbService.Photos.Upload(albumAid, "Uploaded from Facebox", new FileInfo(FileName));
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
+        //        //fbService.Photos.UploadAsync(albumAid, "Uploaded from Facebox", Photo, Enums.FileType.png, UploadCallback, null);
+        //        fbService.Photos.Upload(albumAid, "Uploaded from Facebox", new FileInfo(FileName));
+        //        return true;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return false;
+        //    }
+        //}
 
-        private void CreateAlbumCallback(album album, object state, FacebookException e)
-        {
+        //private void CreateAlbumCallback(album album, object state, FacebookException e)
+        //{
             
-        }
+        //}
 
-        private void UploadCallback(photo p, object state, FacebookException e)
-        {
+        //private void UploadCallback(photo p, object state, FacebookException e)
+        //{
             
-        }
+        //}
 
-        private void PhotoTagger(string photoPid)
-        {
+        //private void PhotoTagger(string photoPid)
+        //{
             
-        }
+        //}
         #endregion
     }
 }
