39e63aa0338bf87dd181e2cc9ce596bd4dbbb8ff
Drew Gross <drew.a.gross@gmail.com>
2011-01-28 09:36:29 -0500

    added stup constructor for watched folder

--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/WatchedFolder.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/WatchedFolder.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace Facebook_demonstration
 {
     class WatchedFolder
     {
+        FileSystemWatcher watcher;
+
+        public WatchedFolder(string path)
+        {
+
+        }
     }
 }
