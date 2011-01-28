353bb5625fdea9ec2fdd7c76d908966baa3b4cb1
Drew Gross <drew.a.gross@gmail.com>
2011-01-28 10:36:54 -0500

    added initialzation in WatchedFolder ctor

--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/WatchedFolder.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/WatchedFolder.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using FacebookApplication;
 
 namespace Facebook_demonstration
 {
     class WatchedFolder
     {
         FileSystemWatcher watcher;
+        UpdateHandler handler = new UpdateHandler();
 
         public WatchedFolder(string path)
         {
+            watcher = new FileSystemWatcher(path);
+            watcher.EnableRaisingEvents = true;
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.FileName |
+                                   NotifyFilters.DirectoryName |
+                                   NotifyFilters.LastAccess;
 
+            watcher.Changed += new FileSystemEventHandler(handler.FaceboxWatcher_Changed);
+            //watcher.Renamed += new FileSystemEventHandler(handler.FaceboxWatcher_Renamed);
+            watcher.Created += new FileSystemEventHandler(handler.FaceboxWatcher_Created);
+            watcher.Deleted += new FileSystemEventHandler(handler.FaceboxWatcher_Deleted);
         }
     }
 }
