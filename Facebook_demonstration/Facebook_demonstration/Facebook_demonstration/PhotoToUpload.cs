e5afe9ede9791a69176b40ca2d0e903455985d3b
Drew Gross <drew.a.gross@gmail.com>
2011-01-29 17:06:21 -0500

    added PhotoToUpload.cs

---
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/PhotoToUpload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facebook_demonstration
+{
+    class PhotoToUpload
+    {
+        public string albumName;
+        public string photoPath;
+
+        public PhotoToUpload(string newAlbumName, string newPhotoPath)
+        {
+            albumName = newAlbumName;
+            photoPath = newPhotoPath;
+        }
+    }
+}
