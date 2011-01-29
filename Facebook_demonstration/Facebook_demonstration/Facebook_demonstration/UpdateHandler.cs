6929ff0f1bbea7c326e47927510bb90f10594361
Drew Gross <drew.a.gross@gmail.com>
2011-01-29 17:28:56 -0500

    added a static thread to UploadHandler to upload photos from

--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/UpdateHandler.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/UpdateHandler.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
 using System.Windows.Forms;
 using Facebook_demonstration;
+using System.Threading;
 
 namespace FacebookApplication
 {
     class UpdateHandler
     {
-        static Queue<PhotoToUpload> photosQueue; //not thread safe
+        static Queue<PhotoToUpload> photosQueue = new Queue<PhotoToUpload>(); //not thread safe
+
+        static Thread uploadThread = new Thread(UpdateHandler.UploadPhotos);
+
+        //Only to be used by the uploadThread. Do not call directly.
+        static private void UploadPhotos()
+        {
+            while (true)
+            {
+                if (photosQueue.Count != 0)
+                {
+                    PhotoToUpload curPhoto = photosQueue.Dequeue();
+                }
+            }
+        }
 
         public void FaceboxWatcher_Changed(object sender, FileSystemEventArgs e)
         {
