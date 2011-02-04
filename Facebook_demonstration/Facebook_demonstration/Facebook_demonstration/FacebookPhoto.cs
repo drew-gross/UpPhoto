59369b9ae77d53ac3f05b6207a20939f5b2f3c2c
Drew Gross <drew.a.gross@gmail.com>
2011-02-03 22:38:32 -0500

    added path to AID map for photos to saved data

--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/FacebookPhoto.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/FacebookPhoto.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Serialization;
+using Facebook_demonstration;
+using Facebook.Rest;
+using Facebook.Schema;
+using Facebook.Utility;
+using Facebook.Winforms.Components;
+using FacebookController;
 
 namespace Facebook_demonstration
 {
-    class FacebookPhoto
+    [Serializable()]
+    class FacebookPhoto : ISerializable
     {
+        String photoAID;
+        String photoPath;
+
+        public FacebookPhoto(photo newPhoto)
+        {
+            photoAID = newPhoto.aid;
+        }
+
+        public FacebookPhoto(SerializationInfo info, StreamingContext ctxt)
+        {
+            photoAID = (String)info.GetValue("photoAID", typeof(String));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("photoAID", photoAID);
+        }
     }
 }
--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/SavedData.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/SavedData.cs
@@ -6,10 +6,13 @@ using System.Runtime.Serialization;
 
 namespace Facebook_demonstration
 {
+    class PhotoAIDtoPhotoPathMap : Dictionary<String, String> { }
+
     [Serializable()]
     class SavedData : ISerializable
     {
         List<String> WatchedFolders;
+        PhotoAIDtoPhotoPathMap AllPhotos;
 
         public SavedData(List<String> newWatchList)
         {
@@ -19,11 +22,13 @@ namespace Facebook_demonstration
         public SavedData(SerializationInfo info, StreamingContext ctxt)
         {
             WatchedFolders = (List<String>)info.GetValue("WatchList", typeof(List<String>));
+            AllPhotos = (PhotoAIDtoPhotoPathMap)info.GetValue("AllPhotos", typeof(PhotoAIDtoPhotoPathMap));
         }
 
         public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
         {
             info.AddValue("WatchList", WatchedFolders);
+            info.AddValue("AllPhotos", AllPhotos);
         }
 
         public List<String> SavedWatchedFolders()
