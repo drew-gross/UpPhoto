7286da9855acace7e0eb295e0023eb6c2e13ad4c
Drew Gross <drew.a.gross@gmail.com>
2011-02-18 20:30:04 -0500

    added carlos version of IsImageExtension

--- Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/StringUtils.cs
+++ Facebook_demonstration/Facebook_demonstration/Facebook_demonstration/StringUtils.cs
@@ -7,6 +7,9 @@ namespace UpPhoto
 {
     static class StringUtils
     {
+        public static string[] ImageExtensions = { ".gif", ".jpg", ".png", ".psd", ".tiff", ".jp2", ".iff", ".wbmp", ".xbm" };
+        public static string[] VideoExtensions = { ".3g2", ".3gp", ".3gpp", ".asf", ".avi", ".dat", ".flv", ".m4v", ".mkv", ".mod", ".mov", ".mp4", ".mpe", ".mpeg", ".mpeg4", ".mpg", ".nsv", ".ogm", ".ogv", ".qt", ".tod", ".vob", ".wmv" };
+
         public static String GetFolderFromPath(string path)
         {
             string[] folders = path.Split('\\');
@@ -22,10 +25,9 @@ namespace UpPhoto
             return String.Join(@"\", (string[])folders.Take(folders.Count() - 1).ToArray());
         }
 
-        public static bool IsImageExtension(string extension)
+        public static bool IsImageExtension(string Extension)
         {
-            string[] validExtensions = { @".jpg", @".JPG", @".jpeg", @".JPEG", @".bmp", @".BMP", @".png", @".PNG", @".tiff", @".TIFF", @".raw", @".RAW" };
-            return validExtensions.Contains(extension);
+            return ImageExtensions.Contains(Extension.ToLower());
         }
     }
 }
