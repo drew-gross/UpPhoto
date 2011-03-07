a9d582327b5fc9061d0ffb6f0b9f8e9666e18690
Drew Gross <drew.a.gross@gmail.com>
2011-03-07 03:22:03 -0500

    some non-working symlinkstuff

--- MakeHardLink/Program.cs
+++ MakeHardLink/Program.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace MakeHardLink
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern bool CreateSymbolicLink(string lpSymlinkFileName, string lpTargetFileName, int dwFlags);
+
         static void Main(string[] args)
         {
+            CreateSymbolicLink(@"UpPhoto", Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\UpPhoto", 1);
         }
     }
 }
