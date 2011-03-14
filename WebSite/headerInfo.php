890742288d928846d9c97244ce77439f178dcbac
Drew Gross <drew.a.gross@gmail.com>
2011-03-13 22:59:32 -0400

    added logo to website

--- WebSite/headerInfo.php
+++ WebSite/headerInfo.php
@@ -3,9 +3,6 @@
 	{
 echo <<<HERE
 <title>$titleAppend - UpPhoto: Fast and simple Facebook Photo uploading</title>
-<!-- <link type="text/css" rel="stylesheet" href="styles/reset.css" />
-<link type="text/css" rel="stylesheet" href="styles/960.css" />
-<link type="text/css" rel="stylesheet" href="styles/text.css" /> -->
 <link type="text/css" rel="stylesheet" href="styles/styles.css" />
 <link rel="shortcut icon" href="favicon.ico" />
 $otherHeaders
--- WebSite/linkMenu.php
+++ WebSite/linkMenu.php
@@ -11,10 +11,20 @@ function navlinks($current){
   $links = array("/" => "Home",
   				 "contact.php" => "Contact Us",
   				 "instructions.php" => "How It Works");
+?>
+<div>
+<div style="float: left">
+<a href="/"><img src="UpPhoto_SMALLLOGO_transparent.png" alt="UpPhoto Logo" style="border: none; outline: 0;"/></a>
+</div>
+<?php 
   echo "<ul class=\"navigation\">";
   foreach($links as $link => $name){
     navlink($link, $name, $name == $current);
   }
   echo "</ul>";
+?>
+<div style="clear: both"></div>
+<?php 
 }
 ?>
+</div>
