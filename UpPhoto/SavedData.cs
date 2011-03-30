b390998457c69826fe9db40a6d6a25f4f841912e
Drew Gross <drew.a.gross@gmail.com>
2011-03-29 22:17:52 -0400

    added save data version upgrading and save the IsFirstRun member

--- UpPhoto/SavedData.cs
+++ UpPhoto/SavedData.cs
@@ -15,18 +15,55 @@ namespace UpPhoto
         //I forgot to add one in the first version so checking if the format is the first format will
         // have to involve checking for something that doesn't exist and catching the exception.
 
+        int SaveDataVersion;
+        int MostRecentSaveDataVersion = 2;
+
+        bool IsFirstTimeRun = true;
+
         public SavedData(List<String> newWatchList, Dictionary<PID, String> newAllPhotos)
         {
             WatchedFolders = newWatchList;
             AllPhotos = newAllPhotos;
+            SaveDataVersion = 2;
         }
 
         public SavedData(SerializationInfo info, StreamingContext ctxt)
         {
             lock (this)
             {
-                WatchedFolders = (List<String>)info.GetValue("WatchList", typeof(List<String>));
-                AllPhotos = (Dictionary<PID, String>)info.GetValue("AllPhotos", typeof(Dictionary<PID, String>));
+                try
+                {
+                    SaveDataVersion = (int)info.GetValue("SaveDataVersion", typeof(int));
+                }
+                //change this to the right kind of exception
+                catch (Exception)
+                {
+                    SaveDataVersion = 1;
+                }
+                switch (SaveDataVersion)
+                {
+                    case 1:
+                        LoadSaveDataVersion1(info);
+                        break;
+                    case 2:
+                        LoadSaveDataVersion2(info);
+                        break;
+                    default:
+                        break;
+                }
+                while (SaveDataVersion < MostRecentSaveDataVersion)
+                {
+                    switch (SaveDataVersion)
+                    {
+                        case 1:
+                            ConvertSaveData1ToSaveData2();
+                            continue;
+                        case 2:
+                            break;
+                        default:
+                            throw new SerializationException();
+                    }
+                }
             }
         }
 
@@ -34,6 +71,8 @@ namespace UpPhoto
         {
             info.AddValue("WatchList", WatchedFolders);
             info.AddValue("AllPhotos", AllPhotos);
+            info.AddValue("SaveDataVersion", SaveDataVersion);
+            info.AddValue("IsFirstTimeRun", IsFirstTimeRun);
         }
 
         public List<String> SavedWatchedFolders()
@@ -44,6 +83,25 @@ namespace UpPhoto
         public Dictionary<PID, String> SavedPIDtoPhotoMap()
         {
             return AllPhotos;
+        }
+
+        private void LoadSaveDataVersion1(SerializationInfo info)
+        {
+            WatchedFolders = (List<String>)info.GetValue("WatchList", typeof(List<String>));
+            AllPhotos = (Dictionary<PID, String>)info.GetValue("AllPhotos", typeof(Dictionary<PID, String>));
+        }
+
+        private void LoadSaveDataVersion2(SerializationInfo info)
+        {
+            WatchedFolders = (List<String>)info.GetValue("WatchList", typeof(List<String>));
+            AllPhotos = (Dictionary<PID, String>)info.GetValue("AllPhotos", typeof(Dictionary<PID, String>));
+            IsFirstTimeRun = (bool)info.GetValue("IsFirstTimeRun", typeof(bool));
+        }
+
+        private void ConvertSaveData1ToSaveData2()
+        {
+            IsFirstTimeRun = false;
+            SaveDataVersion = 2;
         }
     }
 }
