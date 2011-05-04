34a520f72a306b0f5b44ee5ea8f40b4629eae4c1
Drew Gross <drew.a.gross@gmail.com>
2011-05-04 18:59:09 -0400

    Revert "reorganizing"
    This reverts commit a94b128908ac9dd38e71975b75821c905b396f82.

---
+++ UpPhoto/FacebookPhoto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using UpPhoto;
+using Facebook.Rest;
+using Facebook.Schema;
+using Facebook.Utility;
+using Facebook.Winforms.Components;
+
+namespace UpPhoto
+{
+    [Serializable()]
+    public class FacebookPhoto : ISerializable
+    {
+        public String pid;
+        public String path;
+
+        public FacebookPhoto(photo newPid, String newPath)
+        {
+            pid = newPid.pid;
+            path = newPath;
+        }
+
+        public FacebookPhoto(SerializationInfo info, StreamingContext ctxt)
+        {
+            pid = (String)info.GetValue("photoAID", typeof(String));
+            pid = (String)info.GetValue("path", typeof(String));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("photoAID", pid);
+            info.AddValue("path", path);
+        }
+    }
+}
