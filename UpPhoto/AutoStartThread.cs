156a47503c6722f7545a482e019aa0a044a7979e
Drew Gross <drew.a.gross@gmail.com>
2011-05-11 20:56:01 -0400

    added autostartthread

---
+++ UpPhoto/AutoStartThread.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UpPhoto
+{
+    class AutoStartThread
+    {
+        Thread thread;
+        public AutoStartThread(ThreadStart newThreadStart, ApartmentState newApartmentState = ApartmentState.MTA)
+        {
+            thread = new Thread(newThreadStart);
+            thread.SetApartmentState(newApartmentState);
+            thread.Start();
+        }
+
+        public void Join()
+        {
+            thread.Join();
+        }
+
+        public void Abort()
+        {
+            thread.Abort();
+        }
+    }
+}
